474688a52ecffa6c514a55d0dd86ea521778d6f0
minindu0 <mw8511@live.com>
2026-01-14 16:41:23 -0600

    update python script for pca and sc3 out

--- python_test/main.py
+++ python_test/main.py
@@ -2,7 +2,6 @@
 import scanpy as sc
 from _consensus import consensus
 import pandas as pd
-import numpy as np
 
 def main():
     # Load dataset
@@ -10,62 +9,64 @@ def main():
     adata_spatial = sc.read_csv("./data/spatial_data.csv")
     print("Data Loaded!")
 
-    # save origin
-    adata_scrna.obs['batch'] = 'scrna'
-    adata_spatial.obs['batch'] = 'spatial'
     # spatial data set cells in coloumns so transform
     adata_spatial = adata_spatial.T
+
     # we have to filter and match the genes on both datasets before concatation
     (scrna_indices, spatial_indices) = get_common_indices()
-
-    print("Before Number of genes (columns) in adata_scrna:", adata_scrna.n_vars)
-    print("Before Number of genes (columns) in adata_spatial:", adata_spatial.n_vars)
 
     # select and rearrange the matrices for common genes
     adata_scrna = adata_scrna[:, scrna_indices]
     adata_spatial = adata_spatial[:, spatial_indices]
-    print("After Number of genes (columns) in adata_scrna:", adata_scrna.n_vars)
-    print("After Number of genes (columns) in adata_spatial:", adata_spatial.n_vars)
-    
-    # preprocess scale and log2(x+1)
-    sc.pp.scale(adata_spatial)
+
+    # preprocess log2(x+1) and scale
     sc.pp.log1p(adata_spatial)
-
-    sc.pp.scale(adata_scrna)
+    sc.pp.scale(adata_spatial)
     sc.pp.log1p(adata_scrna)
-
+    sc.pp.scale(adata_scrna)
     print("Scaled and logged")
+
     # mark them 
     adata_scrna.obs['batch'] = 'scrna'
     adata_spatial.obs['batch'] = 'spatial'
 
     # concat
     adata = adata_scrna.concatenate(adata_spatial, batch_key='batch')
-    adata.X = np.nan_to_num(adata.X, nan=0)
-    X = adata.X
-    # look to see how many. nan
-    print("Contains NaN:", np.isnan(X).any())
-    print("Contains inf:", np.isinf(X).any())
-    print("Min value:", X.min())
-    print("Max value:", X.max())
+
+    # do pca for all 
     sc.tl.pca(adata, n_comps= 50, svd_solver='arpack')
-    print("PCA calculation")
+    print("PCA calculation done")
 
-    
     # separate them
-    adata_scrna_pca = adata[adata.obs['batch'] == 'scrna'].copy()
-    adata_spatial_pca = adata[adata.obs['batch'] == 'spatial'].copy()
+    adata_scrna_pca = adata[adata.obs['batch'] == '0'].copy()
+    adata_spatial_pca = adata[adata.obs['batch'] == '1'].copy()
 
+    # verify
+    print(adata_scrna_pca)
+    print(adata_spatial_pca)
+    
     # run sc3 on both spatial and scrna
-    # get the cluster center
     consensus(adata_scrna_pca, n_clusters = 6)
-    print("Sc3 on scrna")
-
-    # using the cluster centers find which cluster centers the cells are aligned to
     consensus(adata_spatial_pca, n_clusters = 6)
-    print("Sc3 on spatial")
+    print("Sc3 on scrna and spatial")
 
-    # adata_spatial.obs['sc3s_4'].to_csv("sc3s_6.csv", index=False)
+    # save scrna sc3 result
+    adata_scrna_pca.obs['sc3s_6'].to_csv("./data/scrna_sc3_result.csv", index=False)
+
+    # save pca result for scrna
+    pca_df = pd.DataFrame(
+        adata_scrna_pca.obsm["X_pca"],
+        index=adata_scrna_pca.obs_names,
+        columns=[f"PC{i+1}" for i in range(adata_scrna_pca.obsm["X_pca"].shape[1])]
+    )
+    pca_df.to_csv("./data/pca_results_scrna.csv")
+    # save pca result for spatial
+    pca_df = pd.DataFrame(
+        adata_spatial_pca.obsm["X_pca"],
+        index=adata_spatial_pca.obs_names,
+        columns=[f"PC{i+1}" for i in range(adata_spatial_pca.obsm["X_pca"].shape[1])]
+    )
+    pca_df.to_csv("./data/pca_results_spatial.csv")
 
 def get_common_indices():
     print("Getting common indices")
